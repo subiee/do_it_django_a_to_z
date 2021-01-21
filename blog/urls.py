b9b8c0cb7bd19c863978761b9e76e34017b5be00
su_biee <asb2008@naver.com>
2021-01-22 00:59:44 +0900

    내비게이션 바와 푸터 모듈화하기-수정

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
+
     # path('<int:pk>/', views.single_post_page),
     # path('', views.index),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.views.generic import ListView, DetailView
 from .models import Post
 
+
 class PostList(ListView):
     model = Post
     ordering = '-pk'
